46670bd901e10e656661018f2aed054ab6cfe664
Luciano Alves <luas10c@gmail.com>
2023-05-17 04:45:32 -0300

    chore(backend): add database config

---
+++ backend/src/database/migrations/20230516121426_create_users_table/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "tb_users" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "tb_users_pkey" PRIMARY KEY ("id")
+);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,14 @@
 import fastify from 'fastify'
 
+import { prisma } from './database'
+
 async function bootstrap() {
   const app = fastify()
 
-  app.get('/', (request) => {
-    return {
-      message: 'Hello'
-    }
+  app.get('/', async (request) => {
+    const data = await prisma.user.findMany()
+
+    return data
   })
 
   const url = await app.listen({ port: 7000, host: '0.0.0.0' })
