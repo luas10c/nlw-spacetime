a9e9349e85bd98eaafc1c816a07c2f6678c0ebeb
Luciano Alves <luas10c@gmail.com>
2023-05-17 04:45:11 -0300

    chore(frontend): add skeleton frontend

---
+++ frontend/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+    es2020: true
+  },
+  extends: ['prettier'],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error'
+  }
+}
---
+++ frontend/src/app/(main)/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className="relative m-10 h-10 w-10 rounded bg-red-500">
+      <h1>Hello, World!</h1>
+    </div>
+  )
+}
+
+export default Home
