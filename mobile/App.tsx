7291ffddbfd3c4a52cf180b41b754b590c5d8f37
Luciano Alves <luas10c@gmail.com>
2023-05-18 08:47:32 -0300

    feat(mobile): add homepage

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,20 +1,61 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageBackground, Text, TouchableOpacity, View } from 'react-native'
+import { styled } from 'nativewind'
+
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold
+} from '@expo-google-fonts/roboto'
+import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
+
+import Logo from './src/assets/nlw-spacetime-logo.svg'
+import Stripes from './src/assets/stripes.svg'
+
+const StripesElement = styled(Stripes)
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_400Regular,
+    Roboto_700Bold,
+    BaiJamjuree_700Bold
+  })
+
+  if (!hasLoadedFonts) {
+    return null
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground
+      source={require('./src/assets/luz.png')}
+      className="relative flex-1 bg-gray-700 px-8 py-10"
+      imageStyle={{ position: 'absolute', left: '-100%' }}>
+      <StatusBar style="light" translucent />
+      <StripesElement className="absolute left-2" />
+      <View className="flex-1 items-center justify-center gap-6">
+        <Logo />
+        <View className="space-y-2">
+          <Text className="text-center font-title text-2xl leading-tight text-gray-50">
+            Sua cápsula do tempo
+          </Text>
+          <Text className="text-center font-body text-base leading-relaxed text-gray-100">
+            Colecione momentos marcantes da sua jornada e compartilhe (se
+            quiser) com o mundo!
+          </Text>
+        </View>
+
+        <TouchableOpacity
+          activeOpacity={0.7}
+          className="rounded-full bg-green-500 px-5 py-2">
+          <Text className="font-alt text-sm uppercase text-black">
+            Começar cadastrar
+          </Text>
+        </TouchableOpacity>
+      </View>
+
+      <Text className="text-center font-body text-sm leading-relaxed text-gray-200">
+        Feito com 💜 no NLW da Rocketseat
+      </Text>
+    </ImageBackground>
   )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
